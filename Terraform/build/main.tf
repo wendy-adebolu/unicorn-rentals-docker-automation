f67d8f4093e1d230c2a270fbbb3b0c3c41db6465
wendy omotuebe <wendyadebolu@gmail.com>
2023-10-12 10:18:58 +0100

    destroying resources

--- Terraform/build/main.tf
+++ Terraform/build/main.tf
@@ -78,30 +78,7 @@ resource "aws_ecs_service" "wordpress_service" {
   # }
 }
 
-# Define a security group for ECS
-resource "aws_security_group" "ecs_security_group" {
-  name_prefix = "ecs-security-group-imaginary-client"
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
 
 # Define a VPC and other networking resources if needed.
 
