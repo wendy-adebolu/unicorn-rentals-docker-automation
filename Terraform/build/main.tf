5a38e7aff4f7d6034a9871664ec05cd9553df8af
wendy <140172773+wendyadebolu@users.noreply.github.com>
2023-12-10 21:09:47 +0000

    Update main.tf

--- Terraform/build/main.tf
+++ Terraform/build/main.tf
@@ -5,6 +5,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "gw" {
